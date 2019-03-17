313b32d682673f0f013eb83136f73cb5ef56bde9
Douglas Drouillard <drouillard@gmail.com>
2019-03-17 10:52:47 -0400

    Add type to user in team gate

--- examples/loopback-keycloak-demo-app/src/gates/team.gate.ts
+++ examples/loopback-keycloak-demo-app/src/gates/team.gate.ts
@@ -6,7 +6,7 @@
 import {Request, RequestContext, HttpErrors} from '@loopback/rest';
 import {inject, Setter, BindingKey} from '@loopback/core';
 import {Gate} from '@shrinedev/loopback-gate';
-import {KeycloakBindings} from '@shrinedev/loopback-gate-keycloak';
+import {KeycloakBindings, UserProfile} from '@shrinedev/loopback-gate-keycloak';
 import {ROUTE_BINDING} from '../sequence';
 
 export class TeamProfile {
@@ -22,7 +22,7 @@ export class TeamGate implements Gate {
   ) {}
 
   async gate(context: RequestContext, request: Request): Promise<TeamProfile> {
-    const user = context.getSync(KeycloakBindings.CURRENT_USER);
+    const user: UserProfile = context.getSync(KeycloakBindings.CURRENT_USER);
 
     return new Promise<TeamProfile>(async (resolve, reject) => {
       // Get user's teams and see if the one included in path is in the list
